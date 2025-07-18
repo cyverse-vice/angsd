dec4971ca751849fb653d1d7e2a5c7a508138099
CosiMichele <nismichele@gmail.com>
2025-07-18 12:16:22 -0700

    fixed gen filelist

--- angsd/generate_filelist.sh
+++ angsd/generate_filelist.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-pwd
-ls -l
-file /angsd/bams/smallNA06985.mapped.ILLUMINA.bwa.CEU.low_coverage.20111114.bam
-set -x
-
-echo "listed info"
 
 for i in `ls *.bam`;do samtools index $i;done
 
 #echo "indexed bamfiles"
 
-for i in `ls *.bam`; do echo "${i}"; done > bam.filelist
+for i in "$(pwd)"/*.bam; do echo "${i}"; done > bam.filelist
 
 echo "created bam.filelist"
 
